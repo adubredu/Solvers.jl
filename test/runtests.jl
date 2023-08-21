ce1648ac6be2f03019007ef66263bb4967c1f5c2
Alphonsus Adu-Bredu <alphonsusbq436@gmail.com>
2023-08-21 11:10:08 -0400

    added mehrotra unit test

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,6 +6,6 @@ import OSQP
 using Test
 using JLD2
 
-include("test_qp/test_create_qp.jl")
-include("test_qp/test_augmented_lagrangian.jl")
-include("test_qp/test_barrier_method.jl")
+# include("test_qp/test_create_qp.jl")
+# include("test_qp/test_augmented_lagrangian.jl")
+include("test_lp/test_mehrotra.jl")
--- test/test_lp/test_mehrotra.jl
+++ test/test_lp/test_mehrotra.jl
@@ -1,13 +1,26 @@
-using Revise 
-using Solvers
+@testset "Mehrotra LP Problem 1" begin
+    A = [1 0 0 1 0 0;
+        0 1 0 0 1 0;
+        0 0 1 0 0 1;
+        1 1 1 0 0 0;
+        0 0 0 1 1 1.0]
+    b = [8, 5, 2, 6, 9.0]
+    c = [5, 5, 3, 6, 4, 1.0]
 
-A = [1 0 0 1 0 0;
-     0 1 0 0 1 0;
-     0 0 1 0 0 1;
-     1 1 1 0 0 0;
-     0 0 0 1 1 1.0]
-b = [8, 5, 2, 6, 9.0]
-c = [5, 5, 3, 6, 4, 1.0]
+    lp = LinearProgram(A, b, c)
+    x, _, _ = solve_mehrotra(lp; verbose=false)
 
-lp = LinearProgram(A, b, c)
-initial_x, initial_λ, initial_s = solve_mehrotra(lp)
+    using JuMP, GLPK 
+    model = JuMP.Model(GLPK.Optimizer)
+    set_silent(model)
+    m,n = size(A)
+    @variable(model, x_var[1:n])
+    @objective(model, Min, c'*x_var)
+    @constraint(model, A*x_var == b)
+    @constraint(model, x_var .>= 0.0)
+    optimize!(model)
+    x_expected = value.(x_var)
+
+    @show x_expected
+    @test x ≈ x_expected atol = 1e-6
+end
